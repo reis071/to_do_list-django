11c88597df57fdec52f0b35563596623da398e7e
reis071 <reiscorreia071@gmail.com>
2025-02-14 16:53:56 -0300

    add view registerTask

--- toDoListApp/models.py
+++ toDoListApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 import uuid
 # Create your models here.
-class task(models.Model):
+class Task(models.Model):
     
     id = models.UUIDField( default=uuid.uuid4, editable=False, primary_key=True)
     nameTask = models.CharField(max_length=200)
--- toDoListApp/views.py
+++ toDoListApp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from models import Task
 
-def home(request):
-    return render(request, 'toDoListApp/index.html')
-
-def sobre(request):
-    return render(request, 'toDoListApp/teste.html')
+def addTask(request):
+    if request.method == 'POST':
+        nameTask = request.POST['nameTask']
+        description = request.POST['description']
+        dateCreation = request.POST['dateCreation']
+        task = Task(nameTask=nameTask,description=description,dateCreation=dateCreation).save()
+        
+        return render(request, 'toDoListapp/cadastrar.html', {'task': task})
