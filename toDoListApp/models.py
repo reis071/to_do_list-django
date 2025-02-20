44b7358d21f2385b20a21fbcf7445aed48466a15
reis071 <reiscorreia071@gmail.com>
2025-02-20 11:03:32 -0300

    add new parameter in the model Task to field nameTask

--- toDoListApp/models.py
+++ toDoListApp/models.py
@@ -3,6 +3,6 @@ import uuid
 # Create your models here.
 class Task(models.Model):
     id = models.UUIDField( default=uuid.uuid4, editable=False, primary_key=True)
-    nameTask = models.CharField(max_length=200)
-    description = models.TextField()
+    nameTask = models.CharField(max_length=200, null=False, blank=False)
+    description = models.TextField(default='none description')
     dateCreation= models.DateField(auto_now_add=True)
