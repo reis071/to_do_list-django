5390ddceb60b4515e64649f461001a75d4ecb992
reis071 <reiscorreia071@gmail.com>
2025-02-20 13:15:19 -0300

    implemented content in the template index

--- toDoListApp/views.py
+++ toDoListApp/views.py
@@ -5,9 +5,9 @@ def addTask(request):
     if request.method == 'POST':
         nameTask = request.POST.get('nameTask')
         description = request.POST.get('description')
-        dateCreation = request.POST.get('dateCreation')
+        
 
-        if nameTask and description and dateCreation:  # Verifica se os campos não estão vazios
-            Task.objects.create(nameTask=nameTask, description=description, dateCreation=dateCreation)  
-
+        if nameTask and description:  
+            Task.objects.create(nameTask=nameTask, description=description)  
+            return redirect('registerTask')
     return render(request, 'toDoListapp/registerTask.html')
